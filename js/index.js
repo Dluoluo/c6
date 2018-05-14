10368d04db8a0a8c3cc584a8e3b2edc8dac07a93
Dluoluo <luo.jiami@qq.com>
2018-05-14 10:47:25 +0800

    'html-首页'

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 var mySwiper = new Swiper('.content-swiper', {
+    //竖向轮播
     direction: 'vertical',
+    //不循环轮播
     loop: false,
-
+    //渐隐渐现
+    slide: 'fade',
     // 如果需要分页器
     pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // 如果需要前进后退按钮
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        el: '.pagination',
+        //自定义分页器
+        type: 'custom',
+        //点击分页器效果
+        clickable:true
     }
 })        
