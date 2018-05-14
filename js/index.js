01d6153ef3321653d34e80e0f4112c798076d6ed
Dluoluo <luo.jiami@qq.com>
2018-05-14 14:58:35 +0800

    '修改html-首页+html-报名'

--- js/index.js
+++ js/index.js
@@ -11,6 +11,32 @@ var mySwiper = new Swiper('.content-swiper', {
         //自定义分页器
         type: 'custom',
         //点击分页器效果
-        clickable:true
-    }
-})        
+        clickable: true
+    },
+
+    // 如果需要前进后退按钮
+    navigation: {
+        nextEl: '.swiper-button-next'
+    },
+})   
+var myPriceSwiper = new Swiper('.price-table-content', {
+    //竖向轮播
+    direction: 'vertical',
+    //不循环轮播
+    loop: false,
+    //渐隐渐现
+    slide: 'fade',
+    // 如果需要分页器
+    pagination: {
+        el: '.price-pagination',
+        //自定义分页器
+        type: 'custom',
+        //点击分页器效果
+        clickable: true
+    },
+
+    // 如果需要滚动条
+    scrollbar: {
+        el: '.price-scrollbar',
+    },
+})      
